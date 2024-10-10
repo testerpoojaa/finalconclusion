33f319801f2c65b7ebd174d0bf8035215ba10733
testerpoojaa <147801711+testerpoojaa@users.noreply.github.com>
2024-10-10 14:30:22 +0530

    lastfee

---
+++ Car.java
@@ -0,0 +1,25 @@
+package oopss;
+newcdfffff
+public class Car {
+	String color;
+	int price;
+	String enginetype;
+
+	public static void main(String[] args) {
+		Car bmw=new Car();
+		System.out.println(bmw.color="blue");
+		System.out.println(bmw.price=5000000);
+		System.out.println(bmw.enginetype="petrol");
+		System.out.println("=============================");
+		Car benz=new Car();
+		System.out.println(benz.color="white");
+		System.out.println(benz.price=60000);
+		System.out.println(benz.enginetype="petrol");
+		System.out.println("=============================");
+		Car audi=new Car();
+		System.out.println(audi.color="black");
+		System.out.println(audi.price=300000);
+		System.out.println(audi.enginetype="diesel");
+	}
+
+}
